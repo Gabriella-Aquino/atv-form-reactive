1f8dbe26efc85b40a323b40d84794d198c81deca
Gabriella-Aquino <gabyyaquino8@gmail.com>
2026-05-26 18:19:03 -0300

    limpa as mudanças da atividade anterior

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CadastroUsuarioComponent } from './cadastro-usuario-component/cadastro-usuario-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CadastroUsuarioComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('atv-reactiveform');
--- src/app/validators/senhas-iguais.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractControl, ValidationErrors } from "@angular/forms";
-
-export function senhasIguais(group: AbstractControl): ValidationErrors | null{
-  const senha = group.get("senha")?.value
-  const confirmarSenha = group.get("confirmarSenha")?.value
-  
-  return senha === confirmarSenha ? null : {senhasDiferentes: true}  
-}
